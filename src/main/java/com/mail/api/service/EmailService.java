e8e28d5b0376ce018a4afe27bfa9b06d4b6fbd7f
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-05-22 21:15:54 -0300

    adicionando configuração email

--- src/main/java/com/mail/api/service/EmailService.java
+++ src/main/java/com/mail/api/service/EmailService.java
@@ -3,6 +3,7 @@ package com.mail.api.service;
 import com.mail.api.model.Email;
 import com.mail.api.repository.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,9 @@ public class EmailService {
 
     @Autowired
     private EmailRepository repository;
+
+    @Autowired
+    private JavaMailSender emailSender;
 
     public void sendEmail(Email email) {
 
