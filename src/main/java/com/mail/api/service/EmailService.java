d4033925371b28673963f6ab7bec31c204677877
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-05-25 21:51:29 -0300

    Adicionando service de enviar email

--- src/main/java/com/mail/api/service/EmailService.java
+++ src/main/java/com/mail/api/service/EmailService.java
@@ -1,10 +1,15 @@
 package com.mail.api.service;
 
+import com.mail.api.enums.StatusEmail;
 import com.mail.api.model.Email;
 import com.mail.api.repository.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -15,9 +20,24 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendEmail(Email email) {
+    public Email sendEmail(Email email) {
 
+        email.setSendDateEmail(LocalDateTime.now());
 
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(email.getEmailFrom());
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+            emailSender.send(message);
+
+            email.setStatusEmail(StatusEmail.SEND);
+        }catch(MailException e){
+            email.setStatusEmail(StatusEmail.ERROR);
+        }finally {
+            return repository.save(email);
+        }
 
     }
 }
